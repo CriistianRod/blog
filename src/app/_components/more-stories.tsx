516a981d8d7ade313bbe5030697a2eb24285f70a
CriistianRod <cristian.frontend.dev@gmail.com>
2024-10-31 14:08:12 -0500

    Feat: background gradient more stories

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -11,7 +11,7 @@ export function MoreStories({ posts }: Props) {
       {/* <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight dark:text-white">
         Escribo para entender
       </h2> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-10 md:gap-y-16 mb-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-10 md:gap-y-16 mb-16 bg-gradient-radial from-slate-800 from-10% to-slate-900 to-50%">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
